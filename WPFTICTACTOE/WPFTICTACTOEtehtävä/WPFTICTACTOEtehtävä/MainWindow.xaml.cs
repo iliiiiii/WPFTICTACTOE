b377c2fa7216a162ab5b46d0c6faa97c220db0b0
ili <118269361+iliiiiii@users.noreply.github.com>
2023-08-08 12:47:07 +0300

    Lisätty kaikki tictactoe peliin liittyvät tiedostot

---
+++ WPFTICTACTOE/WPFTICTACTOEtehtävä/WPFTICTACTOEtehtävä/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFTICTACTOEtehtävä
+{
+    public partial class MainWindow : Window
+    {
+        private bool playerXTurn = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Content == null)
+            {
+                button.Content = playerXTurn ? "X" : "O";
+                playerXTurn = !playerXTurn;
+                
+            }
+        }
+    }
+}
